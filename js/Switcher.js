7010b12901b5dc71cc7797d5750428a1c6d567cd
BarbanAlberto <9698260@itisrossi.vi.it>
2023-12-10 20:17:44 +0100

    Aggiunto il resto della UI iniziale della partita

--- js/Switcher.js
+++ js/Switcher.js
@@ -3,14 +3,21 @@ class Switcher {
   /** @type {Switcher<["main", "game"]>} */ static #LEVEL = new this({
     main: assertNotNull(document.getElementById("main-menu")),
     game: assertNotNull(document.getElementById("game"))
-  }, true);
+  }, false);
   static get LEVEL() { return this.#LEVEL; }
 
   /** @type {Switcher<["main", "settings"]>} */ static #MAIN_MENU = new this({
     main: assertNotNull(document.getElementById("main")),
     settings: assertNotNull(document.getElementById("settings"))
-  }, false);
+  }, true);
   static get MAIN_MENU() { return this.#MAIN_MENU; }
+
+  /** @type {Switcher<["buildings", "volcano", "museum"]>} */ static #GAME = new this({
+    buildings: assertNotNull(document.getElementById("buildings")),
+    volcano: assertNotNull(document.getElementById("volcano")),
+    museum: assertNotNull(document.getElementById("museum"))
+  }, false);
+  static get GAME() { return this.#GAME; }
 
   #elements;
   #hideClass;
@@ -18,11 +25,11 @@ class Switcher {
 
   /**
    * @param {{[name in TNames[number]]: HTMLElement}} elements
-   * @param {boolean} isLevel
+   * @param {boolean} isDiff
    */
-  constructor(elements, isLevel) {
+  constructor(elements, isDiff) {
     this.#elements = elements;
-    this.#hideClass = isLevel ? "unloaded" : "hidden";
+    this.#hideClass = isDiff ? "hidden" : "unloaded";
 
     const values = Object.values(/** @type {{[name: string]: HTMLElement}} */ (elements));
     this.#shown = assertNotUndefined(values[0]);
