ff862e4293f660695ad0a54a6e01f23ff988e0ce
BarbanAlberto <9698260@itisrossi.vi.it>
2023-12-27 20:09:10 +0100

    Migliorata la gestione dei suoni in contemporanea

--- js/SoundManager.js
+++ js/SoundManager.js
@@ -1,59 +1,60 @@
 class SoundManager {
   static #MAX_VOLUME = 10;
-  static #SOUNDS = {
-    main: this.#createSound("Alexander Nakarada - Battle Of The Creek.m4a", true),
-    ingame: this.#createSound("Age of Empires II - Bass Bag.mp3", true)
-  };
+  static #SOUNDS = /** @type {const} */ ({
+    main: {
+      filename: "Alexander Nakarada - Battle Of The Creek.m4a",
+      music: true
+    },
+    ingame: {
+      filename: "Age of Empires II - Bass Bag.mp3",
+      music: true
+    },
+    creation: {
+      filename: "Creazione Edificio.mp3",
+      music: false
+    },
+    destruction: {
+      filename: "Distruzione Edificio.mp3",
+      music: false
+    }
+  });
+
+  /** @type {Set<HTMLAudioElement>} */ static #playing = new Set();
+  /** @type {boolean} */ static #interacted = false;
 
   /** @type {number} */ static #volume;
   static get volume() { return this.#volume; }
   static set volume(volume) {
     this.#volume = Math.min(Math.max(volume, 0), this.#MAX_VOLUME);
     const scaledVolume = this.#volume / this.#MAX_VOLUME;
-    for (const sound of Object.values(this.#SOUNDS)) sound.volume = scaledVolume;
+    for (const sound of this.#playing) sound.volume = scaledVolume;
   }
 
-  /** @type {Set<HTMLAudioElement> | true} */ static #interacted = new Set();
-  static #stopping = false;
-
-  /** @param {"main" | "ingame"} sound */
+  /** @param {"main" | "ingame" | "creation" | "destruction"} sound */
   static play(sound) {
-    const audio = this.#SOUNDS[sound];
-    if (this.#interacted == true) audio.play();
-    else this.#interacted.add(audio);
+    const audio = this.#instantiate(sound);
+    if (this.#interacted) audio.play();
+    this.#playing.add(audio);
   }
 
   static stop() {
-    if (this.#interacted != true) {
-      for (const sound of this.#interacted) {
-        if (sound.loop) this.#interacted.delete(sound);
-      }
-    } else {
-      this.#stopping = true;
-      for (const sound of Object.values(this.#SOUNDS)) {
-        if (!sound.loop) continue;
+    if (this.#interacted) {
+      for (const sound of this.#playing) {
         sound.pause();
-        sound.currentTime = 0;
+        sound.src = sound.src;
       }
     }
+
+    this.#playing.clear();
   }
 
-  /**
-   * @param {string} filename
-   * @param {boolean} music
-   */
-  static #createSound(filename, music) {
-    const sound = new Audio(`sounds/${filename}`);
-    if (music) {
-      sound.loop = true;
-      sound.addEventListener("pause", () => {
-        if (this.#stopping) {
-          this.#stopping = false;
-          return;
-        }
-        sound.play();
-      });
-    }
+  /** @param {"main" | "ingame" | "creation" | "destruction"} name */
+  static #instantiate(name) {
+    const info = this.#SOUNDS[name];
+    const sound = new Audio(`sounds/${info.filename}`);
+    sound.volume = this.#volume / this.#MAX_VOLUME;
+    sound.loop = info.music;
+    sound.addEventListener("pause", () => sound.currentTime == sound.duration ? this.#playing.delete(sound) : sound.play());
     return sound;
   }
 
@@ -65,8 +66,7 @@ class SoundManager {
       if (e instanceof KeyboardEvent && (e.key.length != 1 && e.key != "Tab")) return;
       removeEventListener("click", onInteraction);
       removeEventListener("keydown", onInteraction);
-      if (this.#interacted == true) throw new TypeError("Assertion failed: interacted was already true");
-      for (const sound of this.#interacted) sound.play();
+      for (const sound of this.#playing) sound.play();
       this.#interacted = true;
     };
 
