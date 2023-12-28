ae6c8b08d53e2c9195d1f180e66eb406f2316ba2
BarbanAlberto <9698260@itisrossi.vi.it>
2023-12-28 19:16:42 +0100

    Aggiunta la gestione dei manufatti

---
+++ js/ArtifactsManager.js
@@ -0,0 +1,44 @@
+class ArtifactsManager {
+  static #NAMES = /** @type {const} */ ([
+    "Anello", "Caraffa", "Moneta", "Monile", "Coppa d'oro",
+    "Statuetta", "Pane", "Rana", "Uomo rannicchiato", "Cavallo"
+  ]);
+  static get TOTAL() { return this.#NAMES.length; }
+  static #ROWS = 2;
+  static #COLUMNS = this.TOTAL / this.#ROWS;
+  static #element = assertNotNull(document.getElementById("museum"));
+  /** @type {{unknown: ChildNode, artifact: Element}[]} */ static #artifacts = [];
+
+  /** @param {number} i */
+  static onArtifactFound(i) {
+    const artifact = assertNotUndefined(this.#artifacts[i]);
+    artifact.unknown.remove();
+    artifact.artifact.classList.remove("unloaded");
+  }
+
+  static {
+    /**
+     * @param {HTMLElement} el
+     * @param {number} i
+     */
+    const setX = (el, i) => el.style.left = `${(i % this.#COLUMNS) * 100 + 120}px`;
+    const artifactTemplate = assertInstanceOf(document.getElementById("artifact"), HTMLTemplateElement).content;
+
+    this.#NAMES.forEach((name, i) => {
+      const unknown = assertInstanceOf(artifactTemplate.cloneNode(true).childNodes[1], HTMLElement);
+      setX(unknown, i);
+      unknown.style.top = `${Math.floor(i / this.#COLUMNS) * 160 + 32}px`;
+
+      const artifact = new Image();
+      artifact.src = `img/${name}.png`;
+      artifact.alt = name;
+      artifact.classList.add("artifact", "unloaded");
+      setX(artifact, i);
+      artifact.style.bottom = `${216 - Math.floor(i / this.#COLUMNS) * 160}px`;
+
+      this.#artifacts.push({unknown, artifact});
+      this.#element.append(unknown);
+      this.#element.append(artifact);
+    });
+  }
+}
--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-((volume, museum, artifactTemplate) => {
+(volume => {
   bindClick("play", () => {
     Switcher.LEVEL.switch("game");
     SoundManager.stop();
@@ -22,21 +22,10 @@
     SoundManager.play("main");
   });
 
-  
-
   bindClick("show-volcano", () => Switcher.GAME.switch("volcano"));
   bindClick("show-museum", () => Switcher.GAME.switch("museum"));
   for (const el of document.getElementsByClassName("back")) {
     assertInstanceOf(el, HTMLElement).addEventListener("click", () => Switcher.GAME.switch("buildings"));
-  }
-
-  for (let y = 0; y < 2; y++) {
-    for (let x = 0; x < 5; x++) {
-      const unknown = assertInstanceOf(artifactTemplate.cloneNode(true).childNodes[1], HTMLElement);
-      unknown.style.left = `${x * 100 + 56}px`;
-      unknown.style.top = `${y * 160 + 32}px`;
-      museum.append(unknown);
-    }
   }
 
   /**
@@ -46,10 +35,6 @@
   function bindClick(id, onClick) {
     assertNotNull(document.getElementById(id)).addEventListener("click", onClick);
   }
-})(
-  assertNotNull(document.getElementById("volume")),
-  assertNotNull(document.getElementById("museum")),
-  assertInstanceOf(document.getElementById("artifact"), HTMLTemplateElement).content
-);
+})(assertNotNull(document.getElementById("volume")));
 
 SoundManager.play("main");
